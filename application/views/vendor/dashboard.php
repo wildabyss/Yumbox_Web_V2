2fa0154105a81d123938db99d08342e5f9985a38
Xinting He <hxth2018@gmail.com>
2016-06-02 17:06:56 -0400

    finished static markup

--- application/views/vendor/dashboard.php
+++ application/views/vendor/dashboard.php
@@ -3,6 +3,7 @@
 	<div class="dshbd_profile">
 		<img src="/imgs/profile image.svg" alt="profile">
 		<div class="dshbd_follow">
+			<h3>Jimmy's Kitchen</h3>
 			<p>100 Followers</p>
 		</div>
 
@@ -14,18 +15,20 @@
 
 		<div class="dshbd_controller">
 		<!-- switch showing -->
-			<button class="btn btn_normal"><h3>OPEN KITCHEN!</h3></button>
-			<button class="btn btn_warning"><h3>CLOSE KITCHEN!</h3></button>
+			<button class="btn btn_activated"><h3>KITCHEN OPENED</h3></button>
+			<button class="btn btn_warning"><h3>KITCHEN CLOSED</h3></button>
 		</div>
 
 		<div class="dshbd_orderFilter">
 			<p>Orders</p>
-			<button class="btn btn_normal">All</button>
-			<button class="btn btn_normal">Current</button>
-			<button class="btn btn_normal">Past</button>
+			<button class="btn btn_activated">All</button>
+			<button class="btn btn_deactivated">Waiting</button>
+			<button class="btn btn_deactivated">Finished</button>
 			
 		</div>
-	</div><div class="dshbd_review">
+	</div>
+
+	<div class="dshbd_review">
 		<div class="dshbd_review_summary">
 			<h3>Hi, Jimmy. It's 13:00, Wed, May 30th. </h3>
 			<p>You have <span class="span_warning"> 2 </span> dishes waiting,</p>
@@ -34,111 +37,61 @@
 		</div>
 
 		<div class="dshbd_review_waiting">
-			<h2 class="afterline">WAITING</h2>
+			<h3 class="afterline warning">WAITING</h3>
 			<div class="order">
 				<div class="order_user">
 					<div class="order_basicInfo clearfix">
 						<div class="floatLeft">
-							<p class="order_name_number">Coral <span>Order No. 999999</span></p>
+							<h2 class="order_name_number">Coral <p>Order No. 999999</p></h2>
 							<h3 class="order_tel">647-888-9999</h3>
 						</div>
 						<p class="order_timeStamp floatRight">Ordered on 11:30am today.</p>
 					</div>
 					<div class="order_deliver_status afterline clearfix">
-						<div class="order_deliverInfo floatLeft">
-							<p class="order_deliver">Deliver/Pick-up</p>
-							<h3 class="order_add">999 QUEEN ST. W, M5V 1A1 </h3>
-						</div>
-						<div class="order_status floatRight">
-							<p class="warning">Haven't deliver yet.</p>
-							<p class="display">Waiting for pick-up.</p>
-							<button class="btn_cancel">cancel the change</button><button class="btn btn_std">SEND</button>
-						</div>
-					</div>
-				</div>
-
-				<div class="order_dishes afterline">
-					<div class="order_item ">
-						<div>
-							<h3 class="order_item_name">Best steak in the town</h3>
-							<h3 class="order_item_altname">滋滋牛排</h3>
-							<h3>1</h3>
-						</div>
-						<div class="order_requirement">
-							<p>+Medium Rare</p>
-							<p>+w/ smash potato </p>
-						</div>
-						<div class="order_item_price">
-							<h3>$2</h3>
-						</div>
-					</div>
-					<div class="order_item ">
-						<div>
-							<h3 class="order_item_name">Best steak in the town</h3>
-							<h3 class="order_item_altname">滋滋牛排</h3>
-							<h3>1</h3>
-						</div>
-						<div class="order_requirement">
-							<p>+Medium Rare</p>
-							<p>+w/ smash potato </p>
-						</div>
-						<div class="order_item_price">
-							<h3>$2</h3>
-						</div>
-					</div>
-					<div class="order_item ">
-						<div>
-							<h3 class="order_item_name">Best steak in the town</h3>
-							<h3 class="order_item_altname">滋滋牛排</h3>
-							<h3>1</h3>
-						</div>
-						<div class="order_requirement">
-							<p>+Medium Rare</p>
-							<p>+w/ smash potato </p>
-						</div>
-						<div class="order_item_price">
-							<h3>$2</h3>
-						</div>
-					</div>
-				</div>
-				<!-- end of the orders -->
-
-				<div class="order_total">
-					<p class="order_total-item">Total 2 items </p>
-					<p class="order_total_price">Total Price <span>$25($5 delivery fee)</span></p>
-				</div>
-			</div>
-			<button class="btn_print btn"><h3>Print order</h3></button>
-		</div>
-
-		<div class="dshbd_review_finished">
-			<h2 class="afterline">FINISHED</h2>
-			<div class="order">
-				<div class="order_user afterline">
-					<div class="order_basicInfo">
-						<p class="order_name_number">Coral <span>Order No. 999999</span></p>
-						<p class="order_timeStamp">Ordered on 11:30am today.</p>
-						<h3 class="order_tel">647-888-9999</h3>
-					</div>
-					<div class="order_deliver_status">
 						<div class="order_deliverInfo">
 							<p class="order_deliver">Deliver/Pick-up</p>
 							<h3 class="order_add">999 QUEEN ST. W, M5V 1A1 </h3>
 						</div>
 						<div class="order_status">
 							<p class="warning">Haven't deliver yet.</p>
-							<p class="display">Delivered.</p>
-							<button class="btn_cancel">cancel the change</button><button class="btn btn_std">SEND</button>
+							<p class="normal hide">Waiting for pick-up.</p>
+							<button class="btn btn_weak">cancel the change</button><button class="btn btn_activated">SEND</button>
 						</div>
 					</div>
 				</div>
 
 				<div class="order_dishes afterline">
 					<div class="order_item ">
-						<div>
-							<h3 class="order_item_name">Best steak in the town</h3>
-							<h3 class="order_item_altname">滋滋牛排</h3>
-							<h3>1</h3>
+
+						<div class="order_names_number">
+
+							<h3 class="floatLeft">1</h3>
+
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
+
+						</div>
+
+						<div class="order_requirement">
+							<p>+Medium Rare</p>
+							<p>+w/ smash potato </p>
+						</div>
+
+						<div class="order_item_price">
+							<h3>$2</h3>
+						</div>
+
+					</div>
+
+					<div class="order_item ">
+						<div class="order_names_number">
+							<h3 class="floatLeft">1</h3>					
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
 						</div>
 						<div class="order_requirement">
 							<p>+Medium Rare</p>
@@ -148,11 +101,14 @@
 							<h3>$2</h3>
 						</div>
 					</div>
+
 					<div class="order_item ">
-						<div>
-							<h3 class="order_item_name">Best steak in the town</h3>
-							<h3 class="order_item_altname">滋滋牛排</h3>
-							<h3>1</h3>
+						<div class="order_names_number">
+							<h3 class="floatLeft">1</h3>			
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
 						</div>
 						<div class="order_requirement">
 							<p>+Medium Rare</p>
@@ -162,11 +118,135 @@
 							<h3>$2</h3>
 						</div>
 					</div>
+				<!-- end of the orders -->
+				</div>
+
+				<div class="order_total clearfix">
+					<p class="order_total-item floatLeft">Total: <span>3</span> items </p>
+					<p class="order_total_price floatRight">Total Price <span>$11($5 delivery fee)</span></p>
+				</div>
+				<button class="btn_sub btn">Print order</button>
+			</div>
+			<!-- end of the one person's order -->
+
+			<div class="order">
+				<div class="order_user">
+					<div class="order_basicInfo clearfix">
+						<div class="floatLeft">
+							<h2 class="order_name_number">Mia <p>Order No. 999999</p></h2>
+							<h3 class="order_tel">647-888-9999</h3>
+						</div>
+						<p class="order_timeStamp floatRight">Ordered on 11:30am today.</p>
+					</div>
+					<div class="order_deliver_status afterline clearfix">
+						<div class="order_deliverInfo">
+							<p class="order_deliver">Pick-up</p>
+						</div>
+						<div class="order_status">
+							<p class="normal hide">Haven't deliver yet.</p>
+							<p class="normal">Waiting for pick-up.</p>
+							<button class="btn btn_weak">cancel the change</button><button class="btn btn_fade">FINISH</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="order_dishes afterline">
 					<div class="order_item ">
-						<div>
-							<h3 class="order_item_name">Best steak in the town</h3>
-							<h3 class="order_item_altname">滋滋牛排</h3>
-							<h3>1</h3>
+						<div class="order_names_number">
+							<h3 class="floatLeft">1</h3>					
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
+						</div>
+						<div class="order_requirement">
+							<p>+Medium Rare</p>
+							<p>+w/ smash potato </p>
+						</div>
+						<div class="order_item_price">
+							<h3>$2</h3>
+						</div>
+					</div>
+
+				<!-- end of the orders -->
+				</div>
+
+				<div class="order_total clearfix">
+					<p class="order_total-item floatLeft">Total: <span>1</span> item </p>
+					<p class="order_total_price floatRight">Total Price <span>$7($5 delivery fee)</span></p>
+				</div>
+				<button class="btn_sub btn">Print order</button>
+			</div>
+			<!-- end of the one person's order -->
+
+
+		</div>
+
+		<div class="dshbd_review_finished">
+			<h3 class="afterline normal">FINISHED</h3>
+			<div class="order">
+				<div class="order_user">
+					<div class="order_basicInfo clearfix">
+						<div class="floatLeft">
+							<h2 class="order_name_number">Joe <p>Order No. 999999</p></h2>
+							<h3 class="order_tel">647-888-9999</h3>
+						</div>
+						<p class="order_timeStamp floatRight">Ordered on 11:30am today.</p>
+					</div>
+					<div class="order_deliver_status afterline clearfix">
+						<div class="order_deliverInfo">
+							<p class="order_deliver">Pick-up</p>
+						</div>
+						<div class="order_status">
+							<p class="normal hide">Haven't deliver yet.</p>
+							<p class="normal hide">Waiting for pick-up.</p>
+							<button class="btn btn_weak">cancel the change</button><button class="btn btn_sub">FINISHED</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="order_dishes afterline">
+					<div class="order_item ">
+						<div class="order_names_number">
+							<h3 class="floatLeft">1</h3>					
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
+						</div>
+						<div class="order_requirement">
+							<p>+Medium Rare</p>
+							<p>+w/ smash potato </p>
+						</div>
+						<div class="order_item_price">
+							<h3>$2</h3>
+						</div>
+					</div>
+
+					<div class="order_item ">
+						<div class="order_names_number">
+							<h3 class="floatLeft">1</h3>					
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
+						</div>
+						<div class="order_requirement">
+							<p>+Medium Rare</p>
+							<p>+w/ smash potato </p>
+						</div>
+						<div class="order_item_price">
+							<h3>$2</h3>
+						</div>
+					</div>
+
+					<div class="order_item ">
+						<div class="order_names_number">
+							<h3 class="floatLeft">1</h3>					
+							<div class="order_names">
+								<h3 class="order_item_name">Best steak in the town</h3>
+								<h3 class="order_item_altname">滋滋牛排</h3>
+							</div>
 						</div>
 						<div class="order_requirement">
 							<p>+Medium Rare</p>
@@ -179,12 +259,11 @@
 				</div>
 				<!-- end of the orders -->
 
-				<div class="order_total">
-					<p class="order_total-item">Total 2 items </p>
-					<p class="order_total_price">Total Price <span>$25($5 delivery fee)</span></p>
+				<div class="order_total clearfix">
+					<p class="order_total-item floatLeft">Total: <span>3</span> items </p>
+					<p class="order_total_price floatRight">Total Price <span>$11($5 delivery fee)</span></p>
 				</div>
-			</div>
-			<button class="btn_print btn"><h3>Print order</h3></button>
+				<button class="btn_sub btn">Print order</button>
 		</div>
 
 
